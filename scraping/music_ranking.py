142fe530e03aa458ff2f542149edb410dba033f3
hyeminLee7 <hmin10580@gmail.com>
2021-07-22 18:01:31 +0900

    July,22

--- scraping/music_ranking.py
+++ scraping/music_ranking.py
@@ -24,7 +24,7 @@ class MusicRanking(object):
     def get_ranking(self):
         _=0
         soup = BeautifulSoup(self.html, 'lxml')
-        a = soup.find_all(self.tag_name,self.class_name[0])
+        a = soup.find_all(self.tag_name, self.class_name[0])
         t = soup.find_all(self.tag_name, self.class_name[1])
         for i, j in zip(a, t):
             _ += 1
@@ -36,9 +36,17 @@ class MusicRanking(object):
         print(self.titles)
 
     def insert_dict(self):
+        '''
+        # 방법 1
+        for i in range(0, len(self.tag_name)):
+            self.dict[self.titles[i]] = self.artists[i]
+        # 방법 2
+        for i, j in zip(self.titles, self.artists):
+            self.dict[i] = j
+        '''
+        # 방법 3
         for i, j in enumerate(self.titles):
             self.dict[j] = self.artists[i]
-
 
         print(self.dict)
 
@@ -65,10 +73,12 @@ def main():
         if menu == 0:
             break
         elif menu == 1:
+            mr.fname = 'bugs'
             mr.domain = 'https://music.bugs.co.kr/chart/track/realtime/total?'
             mr.query_string = f'chartdate={"20210720"}&charthour={"16"}'
             mr.set_html()
         elif menu == 2:
+            mr.fname = 'melon'
             mr.domain = 'https://www.melon.com/chart/index.htm?'
             mr.query_string = f'dayTime={"2021072111"}'
             mr.set_html()
@@ -82,7 +92,6 @@ def main():
         elif menu == 5:
             mr.dict_to_dataframe()
         elif menu == 6:
-
             mr.df_to_csv()
 
 
