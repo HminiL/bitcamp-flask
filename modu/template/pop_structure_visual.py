fadd6020cb8ce57b181def57c00be33a6c2cc172
hyeminLee7 <hmin10580@gmail.com>
2021-07-28 15:39:30 +0900

    ..

--- modu/template/pop_structure_visual.py
+++ modu/template/pop_structure_visual.py
@@ -2,16 +2,18 @@ import csv
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-
+from matplotlib import font_manager, rc
+rc('font', family=font_manager.FontProperties(fname='C:/Windows/Fonts/H2GTRE.ttf').get_name())
 from modu.template import ChangeTemperatureOnMyBirthday
 
 
 class Population():
 
     data: [] = list()
+    home: [] = list()
 
     def read_data(self):
-        data = csv.reader(open('data/202106_202106_연령별인구현황_월간.csv', encoding='utf-8'))
+        data = csv.reader(open('./data/202106_202106_연령별인구현황_월간_without_comma.csv', encoding='utf-8'))
         next(data)
         self.data = data
         # print([i for i in data])
@@ -31,21 +33,31 @@ class Population():
     def find_similar_dong(self):
         data = self.data
         name = input('인구 구조가 알고 은싶 지역의 이름(읍면동 단위)을 입력해주세요 : ')
+        home = []
         # home = (np.array(i[3:], dtype=int) for i in data if name in i[0])
         for i in data:
             if name in i[0]:
-                home = np.array(i[3:], dtype=int) / int(i[2])
+                self.home = np.array(i[3:], dtype=int) / int(i[2])
 
-        plt.rc('font', family='C:/Windows/Fonts/H2GTRE.ttf')
-        plt.title(name+'지역의 인구구조')
-        plt.plot(home)
+        # plt.rc('font', family='Malgun Gothic')
+
+        print(self.home)
+
+    def array_to_list(self, arr:object) -> []:
+        return arr.to_list()
+
+    def list_to_arrary(self, ls:[]) -> object:
+        return np.array(ls)
+
+    def show_plot_home(self, arr:object):
+        plt.title(f'{self.home} 지역의 인구 구조')
+        plt.plot(arr)
         plt.show()
-
-        print(home)
 
 
 if __name__ == '__main__':
     pop = Population()
     pop.read_data()
     # pop.show_flot(pop.pop_per_dong('역삼2동'))
-    # pop.find_similar_dong()
+    arr_home = pop.list_to_arrary(pop.home)
+    pop.find_similar_dong()
